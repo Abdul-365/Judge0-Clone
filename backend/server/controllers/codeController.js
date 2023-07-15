ba5bd81a6a8e1529b811d69f4956f74ee4563d12
Abdul Mannan <mannansaudi@gmail.com>
2023-07-15 16:56:38 +0530

    added support for compiled lannguages

--- backend/server/controllers/codeController.js
+++ backend/server/controllers/codeController.js
@@ -1,6 +1,7 @@
 import { spawn } from 'child_process';
 import { body, validationResult } from 'express-validator';
 import fs from 'fs';
+import path from 'path';
 import stream from 'stream';
 import Code from '../models/codeModel';
 import languages from './languages.json';
@@ -8,84 +9,103 @@ import languages from './languages.json';
 // -------------------------------- Execute code in sandbox environment --------------------------------
 
 export const executeCode = (req, res) => {
-    const { code, language_id, stdin = '', expected_output = '', time_limit, memory_limit } = req.body;
+    const { source_code, language_id, stdin = '', expected_output = '', time_limit, memory_limit } = req.body;
     const language = languages.find(lang => lang.id === language_id);
 
-    if (!language) {
-        return res.status(400).json({ error: 'Unsupported language', status: 'error' });
-    }
+    if (!language)
+        return res.status(400).json({ error: 'Unsupported Language', status: 'Error' });
 
     // write code to a temporary file
     try {
-        fs.writeFileSync(language.fileName, code);
+        fs.writeFileSync(language.fileName, source_code);
     } catch (err) {
-        return res.status(500).json({ error: 'Failed to write code to file', status: 'error' });
+        return res.status(500).json({ error: 'Failed to write code to file', status: 'Error' });
     }
 
-    // create a readable stream for user input
-    const inputStream = new stream.Readable();
-    inputStream.push(stdin);
-    inputStream.push(null);
-
-    // run code in a Docker container
-    let runCommand = language.runCommand.replace('{fileName}', language.fileName);
-    if (time_limit) {
-        runCommand = `timeout ${time_limit} ${runCommand}`;
+    // compile code if necessary
+    let compile, compile_output = '';
+    if (language.compileCommand) {
+        compile = spawn('docker', ['run', '-i', '--rm', '-v', `${process.cwd()}:/app`, '-w', '/app', language.dockerImage, 'sh', '-c', language.compileCommand]);
+        compile.stderr.on('data', (data) => {
+            compile_output += data.toString();
+        });
+        compile.on('close', (code) => {
+            if (code !== 0)
+                return res.json({ status: 'Compilation Error', compile_output });
+            runCode();
+        });
+    } else {
+        runCode();
     }
-    const run = spawn('docker', ['run', '-i', '--rm', '-v', `${process.cwd()}:/app`, '-w', '/app', language.dockerImage, 'sh', '-c', `time -v ${runCommand}`]);
 
-    // pass user input to container
-    inputStream.pipe(run.stdin);
+    function runCode() {
+        // create a readable stream for user input
+        const inputStream = new stream.Readable();
+        inputStream.push(stdin);
+        inputStream.push(null);
 
-    // read output from child process
-    let stdout = '';
-    let stderr = '';
-    let time = '';
-    let memory = '';
-    let status = '';
-    run.stdout.on('data', (data) => {
-        const dataStr = data.toString();
-        stdout += dataStr;
-    });    
+        // run code in a Docker container
+        let runCommand = language.runCommand.replace('{fileName}', language.fileName);
+        if (time_limit)
+            runCommand = `timeout ${time_limit} ${runCommand}`;
+        const timeOutputFile = path.join('./time-output.txt');
+        const run = spawn('docker', ['run', '-i', '--rm', '-v', `${process.cwd()}:/app`, '-w', '/app', language.dockerImage, 'sh', '-c', `time -v -o ${timeOutputFile} ${runCommand}`]);
 
-    // handle runtime errors
-    run.stderr.on('data', (data) => {
-        const dataStr = data.toString();
-        if (dataStr.includes('Elapsed (wall clock) time')) {
-            time = dataStr.match(/Elapsed \(wall clock\) time.*: (.*)/)[1];
-        }
-        if (dataStr.includes('Maximum resident set size')) {
-            memory = dataStr.match(/Maximum resident set size.*: (.*)/)[1];
-            if (memory_limit && parseInt(memory) > memory_limit) {
-                status = 'Memory limit exceeded';
-            }
-        }
-        if (!dataStr.includes('\tCommand being timed')) {
+        // pass user input to container
+        inputStream.pipe(run.stdin);
+
+        let stdout = '';
+        let stderr = '';
+        let time = '';
+        let memory = '';
+        let status = '';
+        
+        // read output from child process
+        run.stdout.on('data', (data) => {
+            const dataStr = data.toString();
+            stdout += dataStr;
+        });
+
+        // read errors from child process
+        run.stderr.on('data', (data) => {
+            const dataStr = data.toString();
             stderr += dataStr;
-        }
-        if (stderr.includes('Command not found')) {
-            status = 'Compilation error';
-        }
-    });       
+        });
 
-    run.on('error', (err) => {
-        res.status(500).json({ error: 'Failed to execute code', status: 'Internal error' });
-    });
+        run.on('error', (err) => {
+            res.status(500).json({ error: 'Failed to execute code', status: 'Internal Error' });
+        });
 
-    run.on('close', (code) => {
-        if (!status) {
-            if (code === 124)
-                status = 'Time limit exceeded';
-            else if (code !== 0) {
-                status = 'Runtime error';
-            } else if (!expected_output || stdout.trim() === expected_output.trim()) {
-                status = 'Accepted';
-            } else {
-                status = 'Wrong answer';
+        run.on('close', (code) => {
+            let timeOutput;
+            try {
+                timeOutput = fs.readFileSync(timeOutputFile, 'utf8');
+            } catch (err) {
+                return res.status(500).json({ error: 'Failed to read time output file', status: 'Internal Error' });
             }
-        }
-        res.json({ stdout, stderr, time, memory, status });
-    });
+            if (timeOutput) {
+                const timeMatch = timeOutput.match(/Elapsed \(wall clock\) time.*: (.*)/);
+                if (timeMatch) {
+                    time = timeMatch[1];
+                }
+                const memoryMatch = timeOutput.match(/Maximum resident set size.*: (.*)/);
+                if (memoryMatch) {
+                    memory = memoryMatch[1];
+                }
+            }
+            if (code === 124)
+                status = 'Time Limit Exceeded';
+            else if (memory_limit && parseInt(memory) > memory_limit)
+                status = 'Memory Limit Exceeded';
+            else if (code !== 0)
+                status = 'Runtime Error';
+            else if (!expected_output || stdout.trim() === expected_output.trim())
+                status = 'Accepted';
+            else
+                status = 'Wrong Answer';
+            res.json({ stdout, stderr, compile_output, time, memory, status });
+        });
+    }
 };
 
 // -------------------------------- Manage Code Submissions --------------------------------
